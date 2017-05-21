e9e79912e9028f7d5138ddfbba90f99b4f6564f9
Martin Hecht <mrbaseman@gmx.de>
2017-05-21 23:17:08 +0200

    re-add a modified version of the uninstall script for classical output_filter module because the existing one does no longer work in wbce 2.x - this is a fix for the headers already sent error during upgrade-script in #262

---
+++ wbce/modules/output_filter/uninstall.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * @category        modules
+ * @package         output_filter
+ * @author          Christian Sommer, WB-Project, Werner v.d. Decken
+ * @copyright       WebsiteBaker Org. e.V.
+ * @link            http://websitebaker.org/
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @platform        WebsiteBaker 2.8.3
+ * @requirements    PHP 5.3.6 and higher
+ * @version         $Id: uninstall.php 1520 2011-11-09 00:12:37Z darkviper $
+ * @filesource      $HeadURL: svn://isteam.dynxs.de/wb_svn/wb280/tags/2.8.3/wb/modules/output_filter/uninstall.php $
+ * @lastmodified    $Date: 2011-11-09 01:12:37 +0100 (Mi, 09. Nov 2011) $
+ *
+ */
+
+//no direct file access
+if(count(get_included_files())==1) die(header("Location: ../index.php",TRUE,301));
+
+$table = TABLE_PREFIX .'mod_output_filter';
+$database->query("DROP TABLE IF EXISTS `$table`");
